13f31d4ef72753f875f069cc3e09c59b782ab44b
Denis <dr144371@gmail.com>
2025-03-23 22:45:25 +0300

    Add home: favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
+        <link rel="icon" href="/icons/favicon.svg" />
       </Head>
       <main className={roboto.className}>
         <Component {...pageProps} />
