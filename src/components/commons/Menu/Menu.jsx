6bf44dbb29ac609974b48fb76fbb073dc8de8ca0
Denis <dr144371@gmail.com>
2025-04-06 00:19:55 +0300

    Refactor Button: update transition

--- src/components/commons/Menu/Menu.jsx
+++ src/components/commons/Menu/Menu.jsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
@@ -8,7 +7,7 @@ import styles from './Menu.module.scss';
 
 const Menu = ({ items, className }) => {
   return (
-    <nav className={clsx(styles.menu, className)}>
+    <nav className={className}>
       <Flex className={styles.list} gap={50}>
         {items.map((item) => (
           <Link key={item.title} href={item.href} className={styles.link}>
